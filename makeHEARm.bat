7dbe996e88b35c0b27535ff44d4ccfa7474d70e6
wizardxz <wizardxz@gmail.com>
2013-04-17 09:02:06 -0400

    simple directory

--- makeHEARm.bat
+++ makeHEARm.bat
@@ -2,25 +2,29 @@
 REM fix path as necessary
 set path=.\;C:\yagarto\bin;
 
+rmdir build /s /q
+mkdir build
+copy keil\* build
+
 REM assemble with '-g' omitted where we want to hide things in the AXF
-arm-none-eabi-as -g -mcpu=cortex-m4 -o ./obj/startup.o ./source/SimpleStartSTM32F4_01.asm
-arm-none-eabi-as -g -mcpu=cortex-m4 -o ./obj/display_driver.o ./source/drivers/display.asm
-arm-none-eabi-as -g -mcpu=cortex-m4 -o ./obj/switch_driver.o ./source/drivers/switch.asm
-arm-none-eabi-as -g -mcpu=cortex-m4 -o ./obj/framework.o ./source/framework.asm
+arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/startup.o ./source/SimpleStartSTM32F4_01.asm
+arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/display_driver.o ./source/drivers/display.asm
+arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/switch_driver.o ./source/drivers/switch.asm
+arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/framework.o ./source/framework.asm
 
 REM compiling C
-arm-none-eabi-gcc -I./  -c -mthumb -O0 -g -mcpu=cortex-m4 ./source/main.c -o ./obj/main.o
-arm-none-eabi-gcc -I./  -c -mthumb -O0 -g -mcpu=cortex-m4 ./source/biz.c -o ./obj/biz.o
+arm-none-eabi-gcc -I./  -c -mthumb -O0 -g -mcpu=cortex-m4 ./source/main.c -o ./build/main.o
+arm-none-eabi-gcc -I./  -c -mthumb -O0 -g -mcpu=cortex-m4 ./source/biz.c -o ./build/biz.o
 
 REM linking
-arm-none-eabi-gcc -nostartfiles -g -Wl,--no-gc-sections -Wl,-Map,HEARm.map -Wl,-T linkHEARmSTM32F4_01.ld -oHEARm.elf ./obj/startup.o ./obj/display_driver.o ./obj/switch_driver.o ./obj/framework.o ./obj/main.o ./obj/biz.o -lgcc
+arm-none-eabi-gcc -nostartfiles -g -Wl,--no-gc-sections -Wl,-Map,./build/HEARm.map -Wl,-T ./build/linkHEARmSTM32F4_01.ld -o./build/HEARm.elf ./build/startup.o ./build/display_driver.o ./build/switch_driver.o ./build/framework.o ./build/main.o ./build/biz.o -lgcc
 
 REM hex file
-arm-none-eabi-objcopy -O ihex HEARm.elf HEARm.hex
+arm-none-eabi-objcopy -O ihex ./build/HEARm.elf ./build/HEARm.hex
 
 REM AXF file
-copy HEARm.elf HEARm.AXF
+copy build\HEARm.elf build\HEARm.AXF
 pause
 
 REM list file
-arm-none-eabi-objdump -S  HEARm.axf >HEARm.lst
+arm-none-eabi-objdump -S  ./build/HEARm.axf >./build/HEARm.lst
