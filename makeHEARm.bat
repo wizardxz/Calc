1e390f7d9b98d6aebae42021c30933e2f01f1437
wizardxz <wizardxz@gmail.com>
2013-05-06 18:16:30 -0400

    rotary completed

--- makeHEARm.bat
+++ makeHEARm.bat
@@ -7,26 +7,19 @@ mkdir build
 copy keil\* build
 
 REM assemble with '-g' omitted where we want to hide things in the AXF
-arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/startup.o ./source/SimpleStartSTM32F4_01.asm
-arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/display_driver.o ./source/drivers/display.asm
-arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/switch_driver.o ./source/drivers/switch.asm
-arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/rotary_driver.o ./source/drivers/rotary.asm
-arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/framework.o ./source/framework.asm
+arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/startup.o ./src/SimpleStartSTM32F4_01.asm
+arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/display_driver.o ./src/drivers/display.asm
+arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/switch_driver.o ./src/drivers/switch.asm
+arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/rotary_driver.o ./src/drivers/rotary.asm
+arm-none-eabi-as -g -mcpu=cortex-m4 -o ./build/framework.o ./src/framework.asm
 
 REM compiling C
-arm-none-eabi-gcc -I./  -c -mthumb -O0 -g -mcpu=cortex-m4 ./source/main.c -o ./build/main.o
-arm-none-eabi-gcc -I./  -c -mthumb -O0 -g -mcpu=cortex-m4 ./source/biz.c -o ./build/biz.o
+arm-none-eabi-gcc -I./  -c -mthumb -O0 -g -mcpu=cortex-m4 ./src/main.c -o ./build/main.o
+arm-none-eabi-gcc -I./  -c -mthumb -O0 -g -mcpu=cortex-m4 ./src/biz.c -o ./build/biz.o
 
 REM linking
 arm-none-eabi-gcc -nostartfiles -g -Wl,--no-gc-sections -Wl,-Map,./build/HEARm.map -Wl,-T ./build/linkHEARmSTM32F4_01.ld ^
--o./build/HEARm.elf ^
-./build/startup.o ^
-./build/display_driver.o ^
-./build/switch_driver.o ^
-./build/rotary_driver.o ^
-./build/framework.o ^
-./build/main.o ^
-./build/biz.o -lgcc
+-o./build/HEARm.elf ./build/*.o -lgcc
 
 REM hex file
 arm-none-eabi-objcopy -O ihex ./build/HEARm.elf ./build/HEARm.hex
